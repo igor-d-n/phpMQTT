4f65b806fb16d6105dd92b51d55c221a8ac0e50f
dev <dev@dev>
2019-09-04 16:55:37 +0300

    fix

--- examples/publish.php
+++ examples/publish.php
@@ -15,10 +15,15 @@ $mqtt = new Publisher($server, $port, $clientId);
 
 try{
     $mqtt->connect($username, $password);
-    for($i=0; $i<10000; $i++){
-        $now = DateTime::createFromFormat('U.u', microtime(true));
-        $mqtt->publish("example/publishtest", "$i Hello World at " . $now->format("m-d-Y H:i:s.u"));
+    $c = 10000;
+    $t1 = microtime(1);
+    for($i=0; $i<$c; $i++){
+//        $now = DateTime::createFromFormat('U.u', microtime(true));
+//        $mqtt->publish("example/publishtest", "$i Hello World at " . $now->format("m-d-Y H:i:s.u"));
+        $mqtt->publish("example/publishtest", "$i Hello World");
     }
+    $tF = round((microtime(1) - $t1)*1000, 2);
+    echo "published $c messages in {$tF}ms\n";
 } catch (ConnectException $e){
     echo "Connection problems: ". $e->getMessage();
 }
